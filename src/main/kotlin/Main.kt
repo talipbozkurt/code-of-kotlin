c1f54e90407205cf18cc9d372f160280204420d9
Talip BOZKURT <talip.com>
2024-12-10 18:49:21 +0300

    Example of val being read-only, not immutable.

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,36 @@
+package org.example
+
+
+fun main() {
+    calculateAvailableSpace()
+}
+
+//"This code block is an example of val being read-only, not immutable."
+class Box {
+    var width: Int = 20
+    var height: Int = 40
+    var lenght: Int = 50
+    var usedSpace: Int = 0
+
+
+    val avaliableSpace: Int
+        get() {
+            return (width * height * lenght) - usedSpace
+        }
+
+}
+
+fun calculateAvailableSpace() {
+    val box = Box()
+    box.height = 10
+    box.width = 20
+    box.lenght = 5
+    box.usedSpace = 2
+    println(box.avaliableSpace)
+
+    box.height = 30
+    box.width = 10
+    box.lenght = 8
+    box.usedSpace = 0
+    println(box.avaliableSpace)
+}
