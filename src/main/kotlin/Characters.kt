e746e67afd19097a5ab755294c84bb41facc5969
Talip BOZKURT <talip.com>
2024-12-12 12:07:30 +0300

    Examples of escape characters

---
+++ src/main/kotlin/Characters.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val exampleString =
+        "Examples of escape characters in Kotlin:\n" +
+                "\t \\t You can add a space with tab.\n" +
+                "\t \\n You can move to a new line.\n" +
+                "\t \\b You can perform a backspace operation.\n" +
+                "\t \\r You can move to the beginning of the line.\n" +
+                "\t \\' You can use a single quote (') character.\n" +
+                "\t \\\" You can use a double quote (\") character.\n" +
+                "\t \\\\ You can use a backslash (\\) character.\n" +
+                "\t \\\$ You can use a dollar sign ($) character."
+
+    println(exampleString)
+
+}
