f73dbb4c88c823c7f9029f298950fad1ab177aac
Talip BOZKURT <talip.com>
2024-12-12 11:46:50 +0300

    An example of the relationship between char and number

---
+++ src/main/kotlin/Numbers.kt
@@ -0,0 +1,14 @@
+
+private fun main() {
+
+    val charNumber = '6'
+    val contertedCharNUmber = charNumber.code
+    val digitToInt = charNumber.digitToInt() // function for conversion char to Int
+    println("charNumber =  $charNumber")
+    println("contertedCharNumber = $contertedCharNUmber") //output -> 54
+    println("diigitToInt =  $digitToInt") // output -> 6
+    //val concertedCharNumber = charNumber.toInt() example of deplecated function
+
+
+}
+
