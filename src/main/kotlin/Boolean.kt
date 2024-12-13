b20583e72a8e69cec70aa96afa2d9e15121f39f4
Talip BOZKURT <talip.com>
2024-12-13 16:50:43 +0300

    Added examples of Boolean

---
+++ src/main/kotlin/Boolean.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    val isStudent: Boolean = true
+    val isTeacher: Boolean = false
+    val isFirstStudentMale: Boolean? = null
+
+    if (isStudent && isTeacher) {
+
+    }
+    if (isStudent and isTeacher) {
+
+    }
+    if (isStudent || isTeacher) {
+    }
+}
